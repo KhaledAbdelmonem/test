086344d3a949a10df8291dd8949597f531a5441d
KhaledAbdelmonem <khaled.abdelmonem1999@gmail.com>
2024-06-27 06:22:14 +0300

    upate

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       ArrayList<Long> list = new ArrayList<>();
-        for(long i =0;i<1000000000;i++){
+        for(long i =0;i<1000000;i++){
             list.add(i);
         }
 
