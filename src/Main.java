04efd8bcf57a91d2a93e836c8ad34ac472339587
KhaledAbdelmonem <khaled.abdelmonem1999@gmail.com>
2024-06-27 06:15:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      ArrayList<Long> list = new ArrayList<>();
+        for(long i =0;i<1000000000;i++){
+            list.add(i);
+        }
+
+    }
+}
